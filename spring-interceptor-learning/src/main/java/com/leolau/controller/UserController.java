17f9a030a8ecf4f8798d795d68fe0b2b4c254177
1033727528@qq.com <1033727528@qq.com>
2018-05-29 09:29:23 +0800

    测试控制层

---
+++ spring-interceptor-learning/src/main/java/com/leolau/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.leolau.controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户Controller
+ *
+ * @author LeoGao
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/advice")
+    public String testException(@ModelAttribute("msg") String msg) {
+        throw new IllegalArgumentException("参数有误，来自 @ModelAttribute:" + msg);
+    }
+}
