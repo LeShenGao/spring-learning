53a588f31d3f97938be555dfc8b5916067b911ca
LeoGao <1033727528@qq.com>
2018-05-29 22:25:20 +0800

    add annotation

--- spring-interceptor-learning/src/main/java/com/leolau/controller/UserController.java
+++ spring-interceptor-learning/src/main/java/com/leolau/controller/UserController.java
@@ -17,16 +17,33 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/user")
 public class UserController {
 
+    /**
+     * test global exception
+     *
+     * @param msg
+     * @return
+     */
     @RequestMapping("/advice")
     public String testException(@ModelAttribute("msg") String msg) {
         throw new IllegalArgumentException("参数有误，来自 @ModelAttribute:" + msg);
     }
 
+    /**
+     * class level request,need not assign request path
+     *
+     * @param request
+     * @return
+     */
     @RequestMapping(produces = "text/plain;charset=UTF-8")
     public String index(HttpServletRequest request) {
         return "url:" + request.getRequestURI() + " can access";
     }
 
+    /**
+     * find user info
+     *
+     * @return json format user object data,encoding is utf8
+     */
     @RequestMapping(value = "/getUserInfo", produces = "application/json;charset=UTF-8", method = RequestMethod.GET)
     public User getUserInfo() {
         User user = new User();
@@ -37,4 +54,6 @@ public class UserController {
         user.setUsername("三胖~");
         return user;
     }
+
+
 }
