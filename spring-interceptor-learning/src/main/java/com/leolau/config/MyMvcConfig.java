16db080327d0ceff8b63eadddff1f9965de2148a
1033727528@qq.com <1033727528@qq.com>
2018-06-01 10:18:51 +0800

    fileUpload config resolve

--- spring-interceptor-learning/src/main/java/com/leolau/config/MyMvcConfig.java
+++ spring-interceptor-learning/src/main/java/com/leolau/config/MyMvcConfig.java
@@ -4,6 +4,8 @@ import com.leolau.interceptor.RunTimeDateInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -21,7 +23,7 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/views/");
+        viewResolver.setPrefix("/WEB-INF/classes/views/");
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
@@ -29,6 +31,7 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
     /**
      * config static resources visit path
+     *
      * @param registry
      */
     @Override
@@ -50,5 +53,13 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/user/index").setViewName("/index");
+        registry.addViewController("/user/fileUpload").setViewName("/fileUpload");
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setMaxUploadSize(1000000);
+        return commonsMultipartResolver;
     }
 }
